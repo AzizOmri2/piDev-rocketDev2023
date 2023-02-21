dded1c54f45bce770361c4706999492e6116a8c4
ahmeddba <ahmed.benabid@esprit.tn>
2023-02-21 11:13:37 +0100

    Crud Finale

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
 {
@@ -15,6 +15,17 @@ class Comment
     private ?int $id = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(
+
+        message: "Comments shouldn't be blank",
+    )]
+    #[Assert\Length(
+
+        max:200,
+        maxMessage:"Your comment shouldn't contain more than {{ limit }} caracters",
+
+
+    )]
     private ?string $description = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
